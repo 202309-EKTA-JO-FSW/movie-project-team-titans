b863e6019b187e5822066b96ee054cefcd92088d
MahmoudRumaneh <mahmoudrmn@gmail.com>
2023-12-02 01:57:27 +0300

    navbar done

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Movies from "./movies";
+import Actors from "./actors";
+import Search from "./search";
+import Genres from "./genres";
+
+function Navbar() {
+  const [moviesDropdownVisible, setMoviesDropdownVisible] = useState(false);
+  const [genresDropdownVisible, setGenresDropdownVisible] = useState(false);
+
+  return (
+    <div className="bg-gray-800">
+      <nav className="flex items-center p-2 text-white">
+
+        <img src="/logo.png" className="w-25 h-15" alt="logo" />
+        <div className="flex items-center space-x-24 justify-between p-4 text-white">
+
+        <Search />
+        <div className="group inline-block relative">
+          <button
+            className="text-white focus:outline-none bg-transparent"
+            onClick={() => setMoviesDropdownVisible(!moviesDropdownVisible)}
+            onBlur={() => setMoviesDropdownVisible(false)}
+          >
+            Movies
+          </button>
+          {moviesDropdownVisible && (
+            <div className="absolute bg-gray-800 text-white p-2 mt-2 rounded-md">
+              <Movies />
+            </div>
+          )}
+        </div>
+
+        <div className="group inline-block relative">
+          <button
+            className="text-white focus:outline-none"
+            onClick={() => setGenresDropdownVisible(!genresDropdownVisible)}
+            onBlur={() => setGenresDropdownVisible(false)}
+          >
+            Genres
+          </button>
+          {genresDropdownVisible && (
+            <div className="absolute bg-gray-800 text-white p-2 mt-2 rounded-md">
+              <Genres />
+            </div>
+          )}
+        </div>
+
+        <Actors />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Navbar/genres.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+export default function Genres() {
+  const [moviesGenres, setMoviesGenres] = useState([]);
+
+  useEffect(() => {
+    const fetchMoviesGenres = async () => {
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTBiNjEzZDU2ZjM3YzM5ODBmMDE3MjkxMTE2Y2Q4NyIsInN1YiI6IjY1NjUyYmE2YTM1YzhlMDBmZTRhMmU3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_GBCLDOufr5pWrAD4IBAu1o6XUAi_3oCo6VYxxe4HI'
+        },
+      };
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/genre/movie/list",
+          options
+        );
+        const data = await response.json();
+        
+        //console.log("API Response:", data); 
+
+        setMoviesGenres(data.genres || []);
+      } catch (error) {
+        console.error("Error fetching genres:", error);
+      }
+    };
+
+    fetchMoviesGenres();
+  }, []);
+  const columnCount = 2; // Set the number of columns
+  const optionsPerColumn = Math.ceil(moviesGenres.length / columnCount);
+
+  return (
+    <div className="flex">
+      {Array.from({ length: columnCount }).map((_, columnIndex) => (
+        <div key={columnIndex} className="flex-1 mr-4">
+          {moviesGenres
+            .slice(columnIndex * optionsPerColumn, (columnIndex + 1) * optionsPerColumn)
+            .map((genre) => (
+              <option key={genre.id} value={genre.name}>
+                {genre.name}
+              </option>
+            ))}
+        </div>
+      ))}
+    </div>
+  );
+}

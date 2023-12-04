e8cb33d327f9b856fa7f9832915df53a1968413e
LunarSalameh <lun20190983@std.psut.edu.jo>
2023-12-04 11:50:49 +0300

    Styling changes to the DropDown Lists

--- src/components/Navbar/genres.jsx
+++ src/components/Navbar/genres.jsx
@@ -1,50 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 
 export default function Genres() {
-  const [moviesGenres, setMoviesGenres] = useState([]);
+  const [moviesGenres, setMoviesGenres] = useState([])
 
   useEffect(() => {
     const fetchMoviesGenres = async () => {
       const options = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTBiNjEzZDU2ZjM3YzM5ODBmMDE3MjkxMTE2Y2Q4NyIsInN1YiI6IjY1NjUyYmE2YTM1YzhlMDBmZTRhMmU3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_GBCLDOufr5pWrAD4IBAu1o6XUAi_3oCo6VYxxe4HI'
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTBiNjEzZDU2ZjM3YzM5ODBmMDE3MjkxMTE2Y2Q4NyIsInN1YiI6IjY1NjUyYmE2YTM1YzhlMDBmZTRhMmU3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_GBCLDOufr5pWrAD4IBAu1o6XUAi_3oCo6VYxxe4HI",
         },
-      };
+      }
       try {
         const response = await fetch(
           "https://api.themoviedb.org/3/genre/movie/list",
-          options
-        );
-        const data = await response.json();
-        
-        //console.log("API Response:", data); 
+          options,
+        )
+        const data = await response.json()
 
-        setMoviesGenres(data.genres || []);
+        //console.log("API Response:", data);
+
+        setMoviesGenres(data.genres || [])
       } catch (error) {
-        console.error("Error fetching genres:", error);
+        console.error("Error fetching genres:", error)
       }
-    };
+    }
 
-    fetchMoviesGenres();
-  }, []);
-  const columnCount = 2; // Set the number of columns
-  const optionsPerColumn = Math.ceil(moviesGenres.length / columnCount);
+    fetchMoviesGenres()
+  }, [])
+  const columnCount = 2 // Set the number of columns
+  const optionsPerColumn = Math.ceil(moviesGenres.length / columnCount)
 
   return (
     <div className="flex">
       {Array.from({ length: columnCount }).map((_, columnIndex) => (
-        <div key={columnIndex} className="flex-1 mr-4">
+        <div key={columnIndex} className="flex-1 ">
           {moviesGenres
-            .slice(columnIndex * optionsPerColumn, (columnIndex + 1) * optionsPerColumn)
+            .slice(
+              columnIndex * optionsPerColumn,
+              (columnIndex + 1) * optionsPerColumn,
+            )
             .map((genre) => (
-              <option key={genre.id} value={genre.name}>
+              <option
+                key={genre.id}
+                className="my-2  mx-1 rounded-2xl px-5  hover:bg-gray-600"
+                value={genre.name}
+              >
                 {genre.name}
               </option>
             ))}
         </div>
       ))}
     </div>
-  );
+  )
 }
--- src/components/Navbar/movies.jsx
+++ src/components/Navbar/movies.jsx
@@ -1,15 +1,59 @@
-import React from "react";
+import React from "react"
+
 function Movies() {
-    return (
-        <div className="movies">
-            <option value="All">Movies</option>
-            <option value="Top Rate">Top Rate</option>
-            <option value="Popular">Popular</option>
-            <option value="Latest">Latest</option>
-            <option value="Now Playing">Now Playing</option>
-            <option value="Upcoming">Upcoming</option>
-        </div>
-      );
-    }
-    
-export default Movies;
+  return (
+    <div className="movies relative">
+      <a href="#">
+        <option
+          className="my-1 mx-1 rounded-2xl px-5 active:bg-gray-600 hover:bg-gray-600"
+          value="All"
+        >
+          Movies
+        </option>
+      </a>
+      <a href="#">
+        <option
+          className="  my-2  mx-1 rounded-2xl px-5 active:bg-gray-600  hover:bg-gray-600"
+          value="Top Rate"
+        >
+          Top Rated
+        </option>
+      </a>
+
+      <a href="#">
+        <option
+          className="my-2  mx-1 rounded-2xl px-5 active:bg-gray-600 hover:bg-gray-600"
+          value="Popular"
+        >
+          Popular
+        </option>
+      </a>
+      <a href="#">
+        <option
+          className="my-2  mx-1 rounded-2xl px-5 active:bg-gray-600 hover:bg-gray-600"
+          value="Latest"
+        >
+          Latest
+        </option>
+      </a>
+      <a href="#">
+        <option
+          className="my-2  mx-1 rounded-2xl px-5 active:bg-gray-600 hover:bg-gray-600"
+          value="Now Playing"
+        >
+          Now Playing
+        </option>
+      </a>
+      <a href="#">
+        <option
+          className="my-2  mx-1 rounded-2xl px-5 active:bg-gray-600 hover:bg-gray-600"
+          value="Upcoming"
+        >
+          Upcoming
+        </option>
+      </a>
+    </div>
+  )
+}
+
+export default Movies
